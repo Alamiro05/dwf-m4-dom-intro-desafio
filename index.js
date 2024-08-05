32e979ff3b5e8016620b4fdf0e93ef024207065e
Alamiro Ignacio Farias Moreno <116329151+Alamiro05@users.noreply.github.com>
2024-08-05 16:47:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ const cosasQueAprendimos = [
   },
 ];
 
+const lista = document.querySelector("ul");
+
+const elementosLi = lista.querySelectorAll("li");
+
+elementosLi.forEach((li) => {
+  lista.removeChild(li);
+});
+
+cosasQueAprendimos.forEach((item) => {
+  const li = document.createElement("li");
+  li.textContent = item.tema;
+  if (item.class) {
+    li.classList.add(item.class);
+  }
+  lista.appendChild(li);
+});
 function main() {}
 
 main();
